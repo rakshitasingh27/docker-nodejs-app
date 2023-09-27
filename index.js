3b896a77e47c18610f1442ea2bf5aa818e4b49b8
Rakshita <132739310+rakshitasingh27@users.noreply.github.com>
2023-09-27 18:28:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const HOST = '0.0.0.0';
-const PORT = 8081;
+const PORT = 8082;
 
 app.get('/', (req, res) => res.send('Hello, World!'));
 
